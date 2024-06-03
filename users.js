465d5bf0e04f212c498e387b080172902382aaef
hajhosseinico <hajhosseinico@gmail.com>
2024-06-03 17:45:01 -0400

    A

--- users.js
+++
@@ -1,9 +0,0 @@
-cat > routes/users.js << EOL
-const express = require('express');
-const router = express.Router();
-const db = require('../db');
-
-// Placeholder for user routes
-
-module.exports = router;
-EOL
