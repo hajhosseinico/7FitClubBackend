c32b205eb6d799b5b4ca48168924ba899559d469
hajhosseinico <hajhosseinico@gmail.com>
2024-06-03 16:49:11 -0400

    A

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: '172.18.0.2', // Use the correct IP address
+const db = mysql.createConnection({
+  host: 'mariadb_new',
   user: 'root',
   password: 'my_password',
-  database: '7FitClubDB'
+  database: '7FitClubDB',
 });
 
-connection.connect(err => {
+db.connect((err) => {
   if (err) {
+    console.error('Error connecting to the database:', err);
     throw err;
   }
   console.log('Connected to the database');
 });
 
-module.exports = connection;
+module.exports = db;
