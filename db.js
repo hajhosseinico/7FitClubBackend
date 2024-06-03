1e2833d6b48bc11b514f4d762153e1b37951efdf
hajhosseinico <hajhosseinico@gmail.com>
2024-06-03 18:35:31 -0400

    A

--- db.js
+++ db.js
@@ -1,17 +1,36 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+
+const connectionConfig = {
     host: 'mariadb_new',
     user: 'root',
     password: 'my_password',
     database: '7FitClubDB'
-});
+};
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the database');
-});
+let connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(connectionConfig);
+
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to the database:', err);
+            setTimeout(handleDisconnect, 2000); // Retry connection after 2 seconds
+        } else {
+            console.log('Connected to the database');
+        }
+    });
+
+    connection.on('error', err => {
+        console.error('Database connection error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET' || err.code === 'ENOTFOUND') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = connection;
