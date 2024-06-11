238b7487ad35d29700bdcd42d4dba94f5476b96e
hajhosseinico <hajhosseinico@gmail.com>
2024-06-11 18:35:09 -0400

    A

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const mysql = require('mysql');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
-const secretKey = 'your_secret_key'; // Change this to a strong secret key
-
-// Middleware to parse JSON
-app.use(express.json());
-
 const corsOptions = {
-    origin: ['http://7fitclub.com', 'http://www.7fitclub.com', 'http://3.133.158.10'],
+    origin: ['http://7fitclub.com', 'http://www.7fitclub.com', 'http://18.118.119.144'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -21,6 +13,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions)); // Use CORS middleware
+app.use(express.json()); // Middleware to parse JSON
 
 // Import Routes
 const authRoutes = require('./routes/auth');
@@ -29,10 +22,9 @@ const calendarRoutes = require('./routes/calendar');
 
 // Use Routes
 app.use('/auth', authRoutes);
-app.use('/users', userRoutes); // Ensure this is correctly registered
+app.use('/users', userRoutes);
 app.use('/calendar', calendarRoutes);
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
