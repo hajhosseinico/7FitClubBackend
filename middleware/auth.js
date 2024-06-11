20783e2fa9d3faf569f9c26775b278847f33ebff
hajhosseinico <hajhosseinico@gmail.com>
2024-06-11 13:14:30 -0400

    A

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
-const secretKey = 'your_secret_key'; // Ensure this matches the secret used when generating the token
+const secretKey = 'your_secret_key';
 
 const auth = (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
-    }
+    const token = req.header('Authorization').split(' ')[1];
+    if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+
     try {
         const decoded = jwt.verify(token, secretKey);
-        console.log('Decoded Token:', decoded); // Log the decoded token
         req.user = decoded;
+        console.log('Decoded User:', req.user); // Log the decoded user
         next();
     } catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
+        console.error('Token verification error:', err); // Log the error
+        res.status(403).json({ msg: 'Token is not valid' });
     }
 };
 
