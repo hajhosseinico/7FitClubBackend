ff60ad0e4e29adf67a1f8f0641e415cfdfc4c473
hajhosseinico <hajhosseinico@gmail.com>
2024-06-11 13:40:09 -0400

    A

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors'); // Import cors
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const secretKey = 'your_secret_key'; // Change this to a strong secret key
 
